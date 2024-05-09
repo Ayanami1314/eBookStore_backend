e5f32f8f61cdc8f6f9f4dd116126a9d0bd7b45c1
Ayanami1314 <1691760166@qq.com>
2024-05-09 21:44:44 +0800

    fix cros bug

--- src/main/java/com/example/ebookstorebackend/cart/CartController.java
+++ src/main/java/com/example/ebookstorebackend/cart/CartController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+//@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 @RestController
 public class CartController {
     @Autowired
--- src/main/java/com/example/ebookstorebackend/order/OrderController.java
+++ src/main/java/com/example/ebookstorebackend/order/OrderController.java
@@ -7,11 +7,14 @@ import com.example.ebookstorebackend.orderitem.OrderItemService;
 import com.example.ebookstorebackend.user.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+//@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 @RestController
 public class OrderController {
     @Autowired
